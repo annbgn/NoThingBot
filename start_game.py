ee27107295f33eb224382bba46f3768abfa212ca
annbgn <0anita0@mail.ru>
2020-02-01 19:23:11 +0300

    Added a check for failure by comparing several screen areas with corresponding grayscaled areas on a results-after-fail screen. Also, restart after failing added

--- start_game.py
+++ start_game.py
@@ -1,9 +1,20 @@
-from pywinauto.keyboard import send_keys
+import ctypes
+import datetime
 import time
+
+from PIL import ImageGrab, ImageOps
+from numpy import *
 from pywinauto.application import Application
-from pprint import pprint
+from pywinauto.keyboard import send_keys
 
 GAMEPATH = r"D:/Steam/steamapps/common/NO THING/no_thing.exe"
+
+SCREENSIZE = (
+    ctypes.windll.user32.GetSystemMetrics(0),
+    ctypes.windll.user32.GetSystemMetrics(1),
+)
+# print(SCREENSIZE)
+
 
 def start_app():
     app = Application(backend="uia").start(GAMEPATH)
@@ -24,11 +35,45 @@ def exit_game():
     send_keys("{ESC}")
 
 
+def restart_game():
+    time.sleep(1)
+    send_keys("{SPACE}")
+
+
+def check_failure() -> bool:
+    red = ImageOps.grayscale(ImageGrab.grab(bbox=(0, 700, 350, 800)))
+    yellow = ImageOps.grayscale(
+        ImageGrab.grab(bbox=(1820, 700, 1920, 800))
+    )  # (SCREENSIZE[0]-100, 700, SCREENSIZE, 800)))
+    cyan = ImageOps.grayscale(ImageGrab.grab(bbox=(0, 1020, 350, 1060)))
+    red_array = array(red.getcolors())
+    yellow_array = array(yellow.getcolors())
+    cyan_array = array(cyan.getcolors())
+
+    r = red_array.sum()
+    y = yellow_array.sum()
+    c = cyan_array.sum()
+    # print ("r = {}, y = {}, c = {} ".format(r,y,c))  -- lets us find out what are the numbers
+
+    if r == 35076 and y == 10191 and c == 14179:
+        print("fail! -- {}".format(datetime.datetime.now()))
+        return True
+    return False
+
+
 def main():
     start_app()
     start_game()
+    time.sleep(5)
+
+    for _ in range(10 * 60):
+        is_fail = check_failure()
+        if is_fail:
+            restart_game()
+        time.sleep(0.1)
+
     exit_game()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
