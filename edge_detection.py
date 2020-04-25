977546bf8093ec08f4920e2654ae77090baf4874
annbgn <fake@mail.com>
2020-04-25 21:17:49 +0300

    minor fixes + refactoring + improve visualisation + add logging

--- edge_detection.py
+++ edge_detection.py
@@ -1,7 +1,12 @@
+import logging
 import time
 
 import cv2
 import numpy
+
+logging.basicConfig(
+    filename="./app.log", format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 
 def detect_edge(image):
@@ -18,46 +23,58 @@ def detect_edge(image):
     v = numpy.median(image)  # v = numpy.mean(image)
 
     # принцип: яркое - уменьшать сигму, темное - увеличить сигму
-    sigma15 = 0.10
-    sigma20 = 0.30
-    sigma25 = 0.50
+    sigma1 = 0.10
+    sigma2 = 0.30
+    sigma3 = 0.50
 
     if v > 255 / 2:
-        sigma15 = sigma15 / 2
-        sigma20 = sigma20 / 2
-        sigma25 = sigma25 / 2
+        sigma1 = sigma1 / 2
+        sigma2 = sigma2 / 2
+        sigma3 = sigma3 / 2
     else:
-        sigma15 = sigma15 * 2
-        sigma20 = sigma20 * 2
-        sigma25 = sigma25 * 2
+        sigma1 = sigma1 * 2
+        sigma2 = sigma2 * 2
+        sigma3 = sigma3 * 2
 
     # apply automatic Canny edge detection using the computed median
-    lower15 = int(max(0, (1.0 - sigma15) * v))
-    upper15 = int(min(255, (1.0 + sigma15) * v))
+    lower1 = int(max(0, (1.0 - sigma1) * v))
+    upper1 = int(min(255, (1.0 + sigma1) * v))
 
-    lower20 = int(max(0, (1.0 - sigma20) * v))
-    upper20 = int(min(255, (1.0 + sigma20) * v))
+    lower2 = int(max(0, (1.0 - sigma2) * v))
+    upper2 = int(min(255, (1.0 + sigma2) * v))
 
-    lower25 = int(max(0, (1.0 - sigma25) * v))
-    upper25 = int(min(255, (1.0 + sigma25) * v))
+    lower3 = int(max(0, (1.0 - sigma3) * v))
+    upper3 = int(min(255, (1.0 + sigma3) * v))
 
-    canny15 = cv2.Canny(cv_image_gray, lower15, upper15)
-    canny20 = cv2.Canny(cv_image_gray, lower20, upper20)
-    canny25 = cv2.Canny(cv_image_gray, lower25, upper25)
+    canny1 = cv2.Canny(cv_image_gray, lower1, upper1)
+    canny3 = cv2.Canny(cv_image_gray, lower3, upper3)
+
+    canny_wo_contrast = cv2.Canny(cv_image_gray, lower2, upper2)
+
     t = time.localtime()
     timestamp = time.strftime("%b-%d-%Y_%H%M%S", t)
     # hist = get_hist(cv_image_gray)
     cv2.imwrite(
         ".\screenshots_\\full{}.jpg".format(timestamp),
-        numpy.hstack(
-            [
-                cv_image,
-                cv2.cvtColor(cv_image_gray_wo_contrast, cv2.COLOR_GRAY2BGR),
-                cv2.cvtColor(cv_image_gray, cv2.COLOR_GRAY2BGR),
-                cv2.cvtColor(canny15, cv2.COLOR_GRAY2BGR),
-                cv2.cvtColor(canny20, cv2.COLOR_GRAY2BGR),
-                cv2.cvtColor(canny25, cv2.COLOR_GRAY2BGR),
-            ]
+        numpy.vstack(
+            (
+                numpy.hstack(
+                    [
+                        cv_image,
+                        cv2.cvtColor(cv_image_gray_wo_contrast, cv2.COLOR_GRAY2BGR),
+                        cv2.cvtColor(cv_image_gray, cv2.COLOR_GRAY2BGR),
+                        numpy.zeros(cv_image.shape, cv_image.dtype),
+                    ]
+                ),
+                numpy.hstack(
+                    [
+                        numpy.zeros(cv_image.shape, cv_image.dtype),
+                        cv2.cvtColor(canny_wo_contrast, cv2.COLOR_GRAY2BGR),
+                        cv2.cvtColor(canny1, cv2.COLOR_GRAY2BGR),
+                        cv2.cvtColor(canny3, cv2.COLOR_GRAY2BGR),
+                    ]
+                ),
+            )
         ),
     )
 
@@ -82,13 +99,17 @@ def is_low_contrast(hist):
             hist,
         )
     )  # тут может оказаться медленно, может надо перерписать обычным фориком или setcomp'ом, но тогда надо позасекать время выполнения кек
-    return bool(result)
+    res = bool(result)
+    logging.info(res)
+    return res
 
 
 def increase_contrast(image):
     """image is b&w"""
     new_image = numpy.zeros(image.shape, image.dtype)
-    alpha = 1.5  # contrast. must be >1
+    mean = image.mean()
+    alpha = 1.5 if mean < 128 else 1.2  # contrast. must be >1
+    logging.info("alpha = {}, mean = {}".format(alpha, mean))
     beta = 0  # brightness. i'm not using it now, but it might be useful for evaluating sigmas for threshold
     for i in range(image.shape[0]):
         new_image[i] = numpy.clip(alpha * image[i] + beta, 0, 255)
